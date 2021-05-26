9f20ee42127635958f697b038ef35066a5150c54
JSheleg <jessica.sheleg@gmail.com>
2021-05-26 15:29:38 -0500

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,7 @@ var createTaskEl = function(taskDataObj){
 
     taskDataObj.id = taskIdCounter;
     tasks.push(taskDataObj);
+    saveTasks()
 
     //create task actions(buttons and select) for task
     var taskActionsEl = createTaskActions(taskIdCounter);
@@ -127,6 +128,8 @@ var completeEditTask = function(taskName, taskType, taskId){
         }
     };
     
+    saveTasks()
+
     alert("Task Updated!");
 
     formEl.removeAttribute("data-task-id");
@@ -194,6 +197,7 @@ var deleteTask = function(taskId){
     }
     //reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+    saveTasks()
 };
 
 var taskStatusChangeHandler = function(event){
@@ -222,8 +226,12 @@ var taskStatusChangeHandler = function(event){
             tasks[i].status = statusValue;
         }
     }
-    console.log(tasks)
+    saveTasks()
 };
+
+var saveTasks = function(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 formEl.addEventListener("submit", taskFormHandler);
 
